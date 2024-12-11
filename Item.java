ab7d4c4817a4f8bf602a9f0856d8a4175075d0cc
feshjust45 <147548885+feshjust45@users.noreply.github.com>
2024-12-12 01:40:33 +0200

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,4 +1,45 @@
 
-public class Item {
+public abstract class Item {
+
+    // private data 
+    private String name;
+    private double price;
+    private int id;
+
+    public Item(int id, String name, double price) {
+        // no default cons will be used 
+        this.id = id;
+        this.name = name;
+        this.price = price;
+
+    }
+
+    //getter and setters
+    /* id getter and setter  */
+    public int getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id = id;
+    }
+
+    /* price getter and setter */
+    public double getprice() {
+        return price;
+    }
+
+    public void setprice() {
+        this.price = price;
+    }
+
+    /* name getter and setter*/
+    public String getname() {
+        return name;
+    }
+
+    public void setname() {
+        this.name = name;
+    }
 
 }
